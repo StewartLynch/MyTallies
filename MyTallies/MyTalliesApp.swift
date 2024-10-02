8dd30a241340a8a786b16c8e345bae5ac8b83cae
Stewart Lynch <slynch@createchsol.com>
2024-10-01 21:08:11 -0700

    Initial Commit

---
+++ MyTallies/MyTalliesApp.swift
@@ -0,0 +1,25 @@
+//
+//----------------------------------------------
+// Original project: MyTallies
+// by  Stewart Lynch on 9/28/24
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2024 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct MyTalliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
